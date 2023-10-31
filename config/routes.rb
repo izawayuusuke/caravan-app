de2abbb607fd2156a596ba99898ca3b58d8cee9a
vagrant <vagrant@localhost.localdomain>
2023-10-31 06:00:21 +0000

    [Start] 4-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+  get 'blogs/show'
+  get 'blogs/new'
+  get 'blogs/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
